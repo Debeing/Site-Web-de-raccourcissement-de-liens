ed2b2770176037265a83f713ebbc0c63ed12fc17
unknown <mbaitelameric@gmail.com>
2025-09-12 12:48:30 +0100

    Initialisation du projet

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Test from './test.tsx'
+
+function App() {
+  return (
+    <div>
+      <h1>bonjour</h1>
+      <Test/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/test.tsx
@@ -0,0 +1,10 @@
+ 
+let name: string = 'Eric';
+name = 'John';
+export default function Test() {
+  return (
+    <div>
+      <h2>{name}</h2>
+    </div>
+  )
+}
