d8245ac4abcdf1a986490fb7c552702b2e74d456
unknown <mbaitelameric@gmail.com>
2025-09-13 03:44:28 +0100

    Finalisation du projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaste } from "@fortawesome/free-solid-svg-icons";
-
-
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 import { useState } from "react";
 
 function App() {
   const [url, setUrl] = useState("");
-  
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(shortUrl);
+  }
+
   const handlePaste = async () => {
     const text = await navigator.clipboard.readText();
     setUrl(text);
   }
+
+  const BITLY_TOKEN = "d23aacfad3d3dbfd4ed81cb0a1ef7f4484e0cdce"; 
+  const handleShorten = async () => {
+    if (!url) return;
+    try {
+      const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${BITLY_TOKEN}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ long_url: url })
+      });
+      const data = await response.json();
+      setShortUrl(data.link || "Erreur Bitly");
+    } catch {
+      setShortUrl("Erreur réseau");
+    }
+  }
+
   return (
     <div>
       <nav className="flex d-flex m-5 align-center">
@@ -33,21 +57,25 @@ function App() {
               <FontAwesomeIcon icon={faPaste} className="cursor-pointer" />
             </span>
           </div>
-          <button type="submit" className="cursor-pointer flex mt-5">
+          <button
+            type="button"
+            className="cursor-pointer flex mt-5"
+            onClick={handleShorten}
+          >
             Raccourcir
           </button>
           <div className="relative mt-5">
             <input
-              type="message"
-              // value={setUrl}
-              // onChange={(e) => setUrl(e.target.value)}
+              type="text"
+              value={shortUrl}
+              readOnly
               placeholder="Votre lien court apparaîtra ici..."
               className="rounded-lg border-2 border-blue-500 focus:border-blue-500 w-80 focus:outline-none pl-10"
             />
-            {/* <span className="absolute right-2 top-1/2 transform -translate-y-1/2 text-blue-500"
+            <span className="absolute right-2 top-1/2 transform -translate-y-1/2 text-blue-500"
              onClick={handleCopy}>
               <FontAwesomeIcon icon={faCopy} className="cursor-pointer" />
-            </span> */}
+            </span>
           </div>
         </div>
       </div>
